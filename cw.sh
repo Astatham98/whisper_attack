271c4ad29d6ae87a8316a1980bfd86b5c414d704
Astatham98 <alex.statham1998@gmail.com>
2026-05-08 11:16:52 +0200

    Update CW and PGD run files

--- cw.sh
+++ cw.sh
@@ -1,21 +1,17 @@
 #! /bin/bash
 NBITER=2000
-SEED=2000
+SEED=42
 LOAD=False
 NAME=cw
 EPS=0.1
 MAXDECR=8
-DATA=test-clean-20
 CONF=0.0
 DECRFACTOR=0.7
 CST=4
 LR=0.01
-python run_attack.py attack_configs/cw.yaml --root=$RSROOT --data_csv_name=$DATA --model_label=tiny --nb_iter=$NBITER --load_audio=$LOAD --seed=$SEED --attack_name=$NAME --eps=$EPS --const=$CST --max_decr=$MAXDECR --lr=$LR --confidence=$CONF --decrease_factor_eps=$DECRFACTOR
-python run_attack.py attack_configs/cw.yaml --root=$RSROOT --data_csv_name=$DATA --model_label=tiny.en --tokenizer_name=gpt2 --nb_iter=$NBITER --load_audio=$LOAD --seed=$SEED --attack_name=$NAME --eps=$EPS --const=$CST --max_decr=$MAXDECR --lr=$LR --confidence=$CONF --decrease_factor_eps=$DECRFACTOR
-python run_attack.py attack_configs/cw.yaml --root=$RSROOT --data_csv_name=$DATA --model_label=base --nb_iter=$NBITER --load_audio=$LOAD --seed=$SEED --attack_name=$NAME --eps=$EPS --const=$CST --max_decr=$MAXDECR --lr=$LR --confidence=$CONF --decrease_factor_eps=$DECRFACTOR
-python run_attack.py attack_configs/cw.yaml --root=$RSROOT --data_csv_name=$DATA --model_label=base.en --tokenizer_name=gpt2 --nb_iter=$NBITER --load_audio=$LOAD --seed=$SEED --attack_name=$NAME --eps=$EPS --const=$CST --max_decr=$MAXDECR --lr=$LR --confidence=$CONF --decrease_factor_eps=$DECRFACTOR
-python run_attack.py attack_configs/cw.yaml --root=$RSROOT --data_csv_name=$DATA --model_label=small --nb_iter=$NBITER --load_audio=$LOAD --seed=$SEED --attack_name=$NAME --eps=$EPS --const=$CST --max_decr=$MAXDECR --lr=$LR --confidence=$CONF --decrease_factor_eps=$DECRFACTOR
-python run_attack.py attack_configs/cw.yaml --root=$RSROOT --data_csv_name=$DATA --model_label=small.en --tokenizer_name=gpt2 --nb_iter=$NBITER --load_audio=$LOAD --seed=$SEED --attack_name=$NAME --eps=$EPS --const=$CST --max_decr=$MAXDECR --lr=$LR --confidence=$CONF --decrease_factor_eps=$DECRFACTOR
-python run_attack.py attack_configs/cw.yaml --root=$RSROOT --data_csv_name=$DATA --model_label=medium --nb_iter=$NBITER --load_audio=$LOAD --seed=$SEED --attack_name=$NAME --eps=$EPS --const=$CST --max_decr=$MAXDECR --lr=$LR --confidence=$CONF --decrease_factor_eps=$DECRFACTOR
-python run_attack.py attack_configs/cw.yaml --root=$RSROOT --data_csv_name=$DATA --model_label=medium.en --tokenizer_name=gpt2 --nb_iter=$NBITER --load_audio=$LOAD --seed=$SEED --attack_name=$NAME --eps=$EPS --const=$CST --max_decr=$MAXDECR --lr=$LR --confidence=$CONF --decrease_factor_eps=$DECRFACTOR
-python run_attack.py attack_configs/cw.yaml --root=$RSROOT --data_csv_name=$DATA --model_label=large --nb_iter=$NBITER --load_audio=$LOAD --seed=$SEED --attack_name=$NAME --eps=$EPS --const=$CST --max_decr=$MAXDECR --lr=$LR --confidence=$CONF --decrease_factor_eps=$DECRFACTOR
+MODEL_LABEL=small
+TEST_SPLIT=vctk-100
+CSV_NAME=vctk-100
+SKIP_PREP=True # Set to True after the first successful run to reuse cached files.
+
+python run_attack.py ./attack_configs/whisper/cw_vctk.yml --root=./data --model_label=$MODEL_LABEL --nb_iter=$NBITER --load_audio=$LOAD --seed=$SEED --attack_name=$NAME --eps=$EPS --const=$CST --max_decr=$MAXDECR --lr=$LR --confidence=$CONF --decrease_factor_eps=$DECRFACTOR --test_splits=$TEST_SPLIT --data_csv_name=$CSV_NAME --skip_prep=$SKIP_PREP
--- pgd.sh
+++ pgd.sh
@@ -1,30 +1,11 @@
 #! /bin/bash
-SEED=100
 NBITER=200
 LOAD=False
-DATA=test-clean-100
 NAME=pgd
 SNR=35
-SEED=235
+SEED=42
+MODEL_LABEL=small
+TEST_SPLIT=vctk-8000
+CSV_NAME=vctk-8000
 
-python run_attack.py pgd.yaml --root=$RSROOT --data_csv_name=$DATA --model_label=small --nb_iter=$NBITER --load_audio=$LOAD --seed=$SEED --attack_name=$NAME --snr=$SNR
-python run_attack.py pgd.yaml --root=$RSROOT --data_csv_name=$DATA --model_label=small.en --tokenizer_name=gpt2 --nb_iter=$NBITER --load_audio=$LOAD --seed=$SEED  --attack_name=$NAME --snr=$SNR
-python run_attack.py pgd.yaml --root=$RSROOT --data_csv_name=$DATA --model_label=medium --nb_iter=$NBITER --load_audio=$LOAD --seed=$SEED --attack_name=$NAME  --snr=$SNR
-python run_attack.py pgd.yaml --root=$RSROOT --data_csv_name=$DATA --model_label=medium.en --tokenizer_name=gpt2 --nb_iter=$NBITER --load_audio=$LOAD --seed=$SEED --attack_name=$NAME --snr=$SNR
-python run_attack.py pgd.yaml --root=$RSROOT --data_csv_name=$DATA --model_label=tiny --nb_iter=$NBITER --load_audio=$LOAD --seed=$SEED --attack_name=$NAME --snr=$SNR
-python run_attack.py pgd.yaml --root=$RSROOT --data_csv_name=$DATA --model_label=tiny.en --tokenizer_name=gpt2 --nb_iter=$NBITER --load_audio=$LOAD --seed=$SEED  --attack_name=$NAME --snr=$SNR
-python run_attack.py pgd.yaml --root=$RSROOT --data_csv_name=$DATA --model_label=base --nb_iter=$NBITER --load_audio=$LOAD --seed=$SEED --attack_name=$NAME --snr=$SNR
-python run_attack.py pgd.yaml --root=$RSROOT --data_csv_name=$DATA --model_label=base.en --tokenizer_name=gpt2 --nb_iter=$NBITER --load_audio=$LOAD --seed=$SEED  --attack_name=$NAME --snr=$SNR
-python run_attack.py pgd.yaml --root=$RSROOT --data_csv_name=$DATA --model_label=large --nb_iter=$NBITER --load_audio=$LOAD --seed=$SEED --attack_name=$NAME --snr=$SNR
-
-SNR=40
-SEED=240
-python run_attack.py pgd.yaml --root=$RSROOT --data_csv_name=$DATA --model_label=small --nb_iter=$NBITER --load_audio=$LOAD --seed=$SEED --attack_name=$NAME --snr=$SNR
-python run_attack.py pgd.yaml --root=$RSROOT --data_csv_name=$DATA --model_label=small.en --tokenizer_name=gpt2 --nb_iter=$NBITER --load_audio=$LOAD --seed=$SEED  --attack_name=$NAME --snr=$SNR
-python run_attack.py pgd.yaml --root=$RSROOT --data_csv_name=$DATA --model_label=medium --nb_iter=$NBITER --load_audio=$LOAD --seed=$SEED --attack_name=$NAME  --snr=$SNR
-python run_attack.py pgd.yaml --root=$RSROOT --data_csv_name=$DATA --model_label=medium.en --tokenizer_name=gpt2 --nb_iter=$NBITER --load_audio=$LOAD --seed=$SEED --attack_name=$NAME --snr=$SNR
-python run_attack.py pgd.yaml --root=$RSROOT --data_csv_name=$DATA --model_label=tiny --nb_iter=$NBITER --load_audio=$LOAD --seed=$SEED --attack_name=$NAME --snr=$SNR
-python run_attack.py pgd.yaml --root=$RSROOT --data_csv_name=$DATA --model_label=tiny.en --tokenizer_name=gpt2 --nb_iter=$NBITER --load_audio=$LOAD --seed=$SEED  --attack_name=$NAME --snr=$SNR
-python run_attack.py pgd.yaml --root=$RSROOT --data_csv_name=$DATA --model_label=base --nb_iter=$NBITER --load_audio=$LOAD --seed=$SEED --attack_name=$NAME --snr=$SNR
-python run_attack.py pgd.yaml --root=$RSROOT --data_csv_name=$DATA --model_label=base.en --tokenizer_name=gpt2 --nb_iter=$NBITER --load_audio=$LOAD --seed=$SEED  --attack_name=$NAME --snr=$SNR
-python run_attack.py pgd.yaml --root=$RSROOT --data_csv_name=$DATA --model_label=large --nb_iter=$NBITER --load_audio=$LOAD --seed=$SEED --attack_name=$NAME --snr=$SNR
+python run_attack.py ./attack_configs/whisper/pgd_vctk.yaml --root=./data --test_splits=$TEST_SPLIT --data_csv_name=$CSV_NAME --model_label=$MODEL_LABEL --nb_iter=$NBITER --load_audio=$LOAD --seed=$SEED --attack_name=$NAME --snr=$SNR
